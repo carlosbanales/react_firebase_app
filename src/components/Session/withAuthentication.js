581155e7311c68678ffe76d2d4bed12fdfd7ca98
Carlos Banales <carloscruzbanales@gmail.com>
2019-08-27 21:47:52 -0700

    basic working app

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -6,24 +6,26 @@ import { withFirebase } from '../Firebase';
 const withAuthentication = Component => {
   class WithAuthentication extends React.Component {
     constructor(props) {
-        super(props);
-  
-        this.state = {
-          authUser: null,
-        };
+      super(props);
+
+      this.state = {
+        authUser: JSON.parse(localStorage.getItem('authUser')),
+      };
     }
 
     componentDidMount() {
-      this.listener = this.props.firebase.auth.onAuthStateChanged(
+      this.listener = this.props.firebase.onAuthUserListener(
         authUser => {
-          authUser
-            ? this.setState({ authUser })
-            : this.setState({ authUser: null });
+          localStorage.setItem('authUser', JSON.stringify(authUser));
+          this.setState({ authUser });
+        },
+        () => {
+          localStorage.removeItem('authUser');
+          this.setState({ authUser: null });
         },
       );
     }
 
-      
     componentWillUnmount() {
       this.listener();
     }
@@ -39,4 +41,5 @@ const withAuthentication = Component => {
 
   return withFirebase(WithAuthentication);
 };
+
 export default withAuthentication;
