ef3010db32fa77e4f1106a19b790864a9346cbab
carlosbanales <carlos@7sixtymedia.com>
2019-12-01 16:58:26 -0800

    added video

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,6 +3,11 @@ import React from 'react';
 const PortfolioPage = () => (
     <div>
         <h1>Portfolio</h1>
+        <iframe
+            width="500" height="300" src="https://www.youtube.com/embed/yKZa0_4L4FU"
+            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+        </iframe>
     </div>
 );
 
