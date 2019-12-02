9be157c0d56ec2141a5ece507f74775b4a005f21
carlosbanales <carlos@7sixtymedia.com>
2019-12-01 18:18:50 -0800

    Adding videos

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,7 +4,22 @@ const PortfolioPage = () => (
     <div>
         <h1>Portfolio</h1>
         <iframe
-            width="500" height="300" src="https://www.youtube.com/embed/yKZa0_4L4FU"
+            width="350" height="200" src="https://www.youtube.com/embed/yKZa0_4L4FU"
+            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+        </iframe>
+        <iframe
+            width="350" height="200" src="https://www.youtube.com/embed/OXfqYHlrJ_Q"
+            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+        </iframe>
+        <iframe
+            width="350" height="200" src="https://www.youtube.com/embed/roldouPjVI4"
+            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+        </iframe>
+        <iframe
+            width="350" height="200" src="https://www.youtube.com/embed/mV01tZoD0fc"
             frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen>
         </iframe>
