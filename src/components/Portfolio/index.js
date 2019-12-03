bdf746365c4c1b7b9e6c5f01fec3d8a0304d212c
Carlos Banales <carloscruzbanales@gmail.com>
2019-12-02 16:16:03 -0800

    Fixed api issue with env variable

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,22 +4,22 @@ const PortfolioPage = () => (
     <div>
         <h1>Portfolio</h1>
         <iframe
-            width="350" height="200" src="https://www.youtube.com/embed/yKZa0_4L4FU"
+            title="pena-922" width="350" height="200" src="https://www.youtube.com/embed/yKZa0_4L4FU"
             frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen>
         </iframe>
         <iframe
-            width="350" height="200" src="https://www.youtube.com/embed/OXfqYHlrJ_Q"
+            title="Nep-Starving" width="350" height="200" src="https://www.youtube.com/embed/OXfqYHlrJ_Q"
             frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen>
         </iframe>
         <iframe
-            width="350" height="200" src="https://www.youtube.com/embed/roldouPjVI4"
+            title="222-6thchakra" width="350" height="200" src="https://www.youtube.com/embed/roldouPjVI4"
             frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen>
         </iframe>
         <iframe
-            width="350" height="200" src="https://www.youtube.com/embed/mV01tZoD0fc"
+            title="222-spiritualmac" width="350" height="200" src="https://www.youtube.com/embed/mV01tZoD0fc"
             frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen>
         </iframe>
