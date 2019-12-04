ff5c421596014b0f4539378f02bc67590d4ab737
Carlos Banales <carloscruzbanales@gmail.com>
2019-12-03 22:58:00 -0800

    added some css

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import './style.css';
 
-const HomePage= () => (
-  <div>
-    <h1>Welcome!</h1>
+const HomePage = () => (
+  <div className='Container'>
+    <div className='LeftBox'>
+      <h3>Design</h3>
+      <p>
+        Expressing your brands values with <br />
+        design and capturing your vision through<br /> thoughtful design.
+      </p>
+    </div>
+
+    <div className='MiddleBox'>
+      <h3>Video Production</h3>
+      <p>
+        We offer a wide variety of Video <br />Production
+        Services that are sure to<br />fit your needs.
+      </p>
+    </div>
+
+    <div className='RightBox'>
+      <h3>Marketing</h3>
+      <p>
+        We offer Social Media Marketing <br />Services where we Create, 
+        Run, and <br />Monitor, Monthly Ad Campaigns on <br />Instagram 
+        and Facebook to help reach <br />your optimal target audience.
+        Contact <br />us for more info on SMM. 
+      </p>
+    </div>
   </div>
 );
 
