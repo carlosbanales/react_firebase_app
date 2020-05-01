ca00ef9962e7af4f75fce8392b0c575b9ad2d38c
Carlos Banales <carlos@7sixtymedia.com>
2020-04-30 21:58:08 -0700

    implemented testing with jest

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -28412,7 +28412,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34599" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43887" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
